049a1ac2dd0854feec1569d87887620a81de6673
zxcvbnma702 <2990452909@qq.com>
2024-02-04 01:13:23 +0800

    硬盘读写

--- OS/x86/boot.asm
+++ OS/x86/boot.asm
@@ -11,14 +11,175 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-xchg bx, bx; bochs 魔术断点
+; xchg bx, bx; bochs 魔术断点
 
 mov si, booting
 call print
 
+mov edi, 0x1000 ; 读取硬盘数据存放的内存位置
+mov ecx, 0 ; 读取硬盘起始扇区
+mov bl, 1 ; 读取硬盘扇区数量 
+
+call read_disk
+
+mov edi, 0x1000; 读取的目标内存
+mov ecx, 2; 起始扇区
+mov bl, 1; 扇区数量
+call write_disk
+
 ; 阻塞
 jmp $
 
+; 读取硬盘
+read_disk:
+
+    ; 第1步，设置要读取的扇区数量。
+    ; 这个数值要写入**0x1f2**端口。
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    ; 第2步，设置起始LBA扇区号。扇区的读写是连续的，因此只需要给出第一个扇区的编号就可以了。
+    ; 28位的扇区号太长，需要将其分成4段，分别写入端口 0x1f3、0x1f4、0x1f5 和 0x1f6。
+    inc dx ; 0x1f3
+    mov al, cl ; 0x1f3号端口存放的是0～7位
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl ; 0x1f4号端口存放的是8～15位
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl ; 0x1f5号端口存放的是16～23位
+    out dx, al
+
+    inc dx ; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111 ; 0x1f6端口的低4位用于存放逻辑扇区号的24～27位
+
+    mov al, 0b1110_0000 ; 第4位用于指示硬盘号，0表示主盘，1表示从盘。高3位是“111”，表示LBA模式。
+    or al, cl
+    out dx, al
+
+    ; 第3步，向端口 0x1f7 写入0x20，请求硬盘读。
+    inc dx ; 0x1f7
+    mov al, 0x20 ; 读硬盘
+    out dx, al
+
+    xor ecx, ecx ; 将 ecx 清空
+    mov cl, bl ; 得到读写扇区的数量
+
+    
+    .read:
+        push cx ; 保存 cx
+        call .waits ; 等待数据准备完毕
+        call .reads ; 读取一个扇区
+        pop cx ; 恢复 cx
+        loop .read
+
+    ret
+    
+    ; 第4步，等待读写操作完成。
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2 ; nop 直接跳转到下一行
+            jmp $+2 ; 一点点延迟
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    ; 第5步，连续取出数据。``0x1f0``是硬盘接口的数据端口，而且还是一个16位端口。
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256 ; 一个扇区 256 字
+        .readw:
+            in ax, dx
+            jmp $+2; 一点点延迟
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
+
+write_disk:
+
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; 起始扇区的前八位
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; 将高四位置为 0
+
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
+
+    inc dx; 0x1f7
+    mov al, 0x30; 写硬盘
+    out dx, al
+
+    xor ecx, ecx; 将 ecx 清空
+    mov cl, bl; 得到读写扇区的数量
+
+    .write:
+        push cx; 保存 cx
+        call .writes; 写一个扇区
+        call .waits; 等待硬盘繁忙结束
+        pop cx; 恢复 cx
+        loop .write
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; nop 直接跳转到下一行
+            jmp $+2; 一点点延迟
+            jmp $+2
+            and al, 0b1000_0000
+            cmp al, 0b0000_0000
+            jnz .check
+        ret
+
+    .writes:
+        mov dx, 0x1f0
+        mov cx, 256; 一个扇区 256 字
+        .writew:
+            mov ax, [edi]
+            out dx, ax
+            jmp $+2; 一点点延迟
+            jmp $+2
+            jmp $+2
+            add edi, 2
+            loop .writew
+        ret
+
+
+;打印 booting 汇编地址处的内存
 print:
     mov ah, 0x0e
 .next:
